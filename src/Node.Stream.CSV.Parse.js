0a9beb46ea6b0771e1d7afa545ba8ec2fdc212eb
Orion Kindel <orionkindel@gmail.com>
2024-05-01 10:01:22 -0500

    fix: more bugs

--- src/Node.Stream.CSV.Parse.js
+++ src/Node.Stream.CSV.Parse.js
@@ -10,21 +10,14 @@ class ParserWithColumns extends Parser {
 /** @type {(s: import('csv-parse').Options) => () => ParserWithColumns} */
 export const makeImpl = c => () => {
   const parser = new ParserWithColumns(c)
-  parser.once('data', columns => {
-    parser.columns = columns;
+  parser.once('readable', () => {
+    parser.columns = parser.read();
   })
   return parser
 }
 
 /** @type {(s: ParserWithColumns) => () => Array<string> | null} */
-export const readImpl = p => () => {
-  const chunk = p.read();
-  if (chunk === p.columns) {
-    return p.read()
-  } else {
-    return chunk
-  }
-}
+export const readImpl = p => () => p.read();
 
 /** @type {(s: ParserWithColumns) => () => Array<string>} */
 export const columnsArrayImpl = p => () => p.columns
