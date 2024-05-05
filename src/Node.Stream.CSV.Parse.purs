bee63db2e7692ec99308c08612b2c84d6c3a5845
Orion Kindel <orionkindel@gmail.com>
2024-05-05 14:15:59 -0500

    fix: race condition or something

--- src/Node.Stream.CSV.Parse.purs
+++ src/Node.Stream.CSV.Parse.purs
@@ -25,7 +25,6 @@ import Effect.Aff (Canceler(..), makeAff)
 import Effect.Aff.Class (class MonadAff, liftAff)
 import Effect.Class (liftEffect)
 import Effect.Exception (error)
-import Effect.Timer (setTimeout)
 import Effect.Uncurried (mkEffectFn1)
 import Foreign (Foreign, unsafeToForeign)
 import Foreign.Object (Object)
@@ -124,7 +123,6 @@ foreach stream cb = do
   liftAff $ makeAff \res -> do
     removeDataListener <- flip (Event.on dataH) stream \row ->
       void
-        $ setTimeout 0
         $ flip catchError (res <<< Left)
         $ do
             cols <- liftMaybe (error "unreachable") =<< getOrInitColumnsMap stream
