3b4ec774147943894e65b7c39aa5ae1b4876ed1e
Orion Kindel <orionkindel@gmail.com>
2024-06-20 15:42:35 -0500

    fix: update node-stream-pipes

--- test/Test/Main.purs
+++ test/Test/Main.purs
@@ -5,10 +5,16 @@ import Prelude
 import Data.Maybe (Maybe(..))
 import Effect (Effect)
 import Effect.Aff (launchAff_)
+import Effect.Class (liftEffect)
+import Effect.Console as Console
+import Node.EventEmitter as Event
+import Node.Process as Process
 import Test.Pipes.CSV as Test.Pipes.CSV
 import Test.Spec.Reporter (specReporter)
 import Test.Spec.Runner (defaultConfig, runSpec')
 
 main :: Effect Unit
-main = launchAff_ $ runSpec' (defaultConfig { failFast = true, timeout = Nothing }) [ specReporter ] do
-  Test.Pipes.CSV.spec
+main = launchAff_ do
+  void $ liftEffect $ Event.on Process.uncaughtExceptionH (const $ Console.error) Process.process
+  runSpec' (defaultConfig { failFast = true, timeout = Nothing }) [ specReporter ] do
+    Test.Pipes.CSV.spec
